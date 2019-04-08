0fd36fbece4061333f7cabd8daaa6f37baa3e44c
Danislav <danito1233@gmail.com>
2019-04-08 13:17:23 +0100

    Set sign_in as root for unauthenticated users

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
   
-  root 'tweets#index'
+  devise_scope :user do
+    authenticated :user do
+      root "tweets#index", as: :authenticated_root
+    end
 
+    unauthenticated do
+      root "devise/sessions#new", as: :unauthenticated_root
+    end
+  end
+
+
+  root 'tweets#index'
+  
   resources :tweets do
   	resources :likes
   end
